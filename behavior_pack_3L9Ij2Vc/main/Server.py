9821f4335df5f25641a1d17ee3be8549858961eb
westernat <westernat233@163.com>
2025-12-21 12:21:38 +0800

    初次运行

--- behavior_pack_3L9Ij2Vc/main/Server.py
+++ behavior_pack_3L9Ij2Vc/main/Server.py
@@ -1,2 +1,8 @@
 # coding=utf-8
 from .QuModLibs.Server import *
+
+@Listen(Events.GameTypeChangedServerEvent)
+def test(args):
+    playerId = args['playerId']
+    msg = compFactory.CreateMsg(playerId)
+    msg.NotifyOneMessage(playerId, str(Entity(playerId).getBox3D().height))
